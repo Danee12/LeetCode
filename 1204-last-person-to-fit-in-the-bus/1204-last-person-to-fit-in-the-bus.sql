1cb91c5efc7eb86b029221fc456ee67761466c2f
danee <70254335+Danee12@users.noreply.github.com>
2024-09-07 15:51:50 +0900

    Time: 682 ms (93.09%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,8 +1,20 @@
 # Write your MySQL query statement below
-SELECT q1.person_name
-FROM Queue q1
-WHERE 1000 >= (SELECT SUM(weight) FROM Queue q2 WHERE q2.turn <= q1.turn)
-ORDER BY turn DESC
-LIMIT 1
-;
+-- -- 서브쿼리 사용
+-- SELECT q1.person_name
+-- FROM Queue q1
+-- WHERE 1000 >= (SELECT SUM(weight) FROM Queue q2 WHERE q2.turn <= q1.turn)
+-- ORDER BY turn DESC
+-- LIMIT 1
+-- ;
 
+-- -- window 함수 사용
+SELECT person_name
+FROM (
+    SELECT *, SUM(weight) OVER(ORDER BY turn) AS totalWeight
+    FROM queue
+) q1
+WHERE totalWeight <= 1000
+ORDER BY totalWeight DESC
+LIMIT 1;
+
+-- -- 
