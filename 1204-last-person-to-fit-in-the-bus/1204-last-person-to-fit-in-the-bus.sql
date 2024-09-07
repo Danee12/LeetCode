4aba91357e03d52824c99be7536c5c0cf8335913
danee <70254335+Danee12@users.noreply.github.com>
2024-09-07 15:59:04 +0900

    Time: 3992 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,13 +1,4 @@
 # Write your MySQL query statement below
--- -- 서브쿼리 사용
--- SELECT q1.person_name
--- FROM Queue q1
--- WHERE 1000 >= (SELECT SUM(weight) FROM Queue q2 WHERE q2.turn <= q1.turn)
--- ORDER BY turn DESC
--- LIMIT 1
--- ;
-
-
 -- -- window 함수 사용
 -- SELECT person_name
 -- FROM (
@@ -19,13 +10,21 @@
 -- LIMIT 1;
 
 
--- -- JOIN 사용 (1650ms)
-SELECT 
-    q1.person_name
-FROM queue q1
-    JOIN queue q2
-    ON q1.turn >= q2.turn
-GROUP BY q1.turn
-HAVING sum(q2.weight) <= 1000
-ORDER BY q1.turn DESC
-LIMIT 1;
+-- -- 서브쿼리 사용
+SELECT q1.person_name
+FROM Queue q1
+WHERE 1000 >= (SELECT SUM(weight) FROM Queue q2 WHERE q2.turn <= q1.turn)
+ORDER BY turn DESC
+LIMIT 1
+;
+
+-- -- JOIN 사용
+-- SELECT 
+--     q1.person_name
+-- FROM queue q1
+--     JOIN queue q2
+--     ON q1.turn >= q2.turn
+-- GROUP BY q1.turn
+-- HAVING sum(q2.weight) <= 1000
+-- ORDER BY q1.turn DESC
+-- LIMIT 1;
